d07396ca281fcbaaa9ccf62d37a0f1ddbd3d3257
CyberShadow7 <80655024+CyberShadow7@users.noreply.github.com>
2021-03-18 19:56:12 -0600

    1.0.0

---
+++ NumbGuess.rb
@@ -0,0 +1,99 @@
+puts "Welcome to this number guessing game\n\n"
+puts "Every time you find the answer in less than a variable number of tries, you will level up to higher ranges"
+puts ""
+time = 0
+puts "Level 1 : [0..2] no decimals; 3 tries"
+numero1 = rand(0..2)
+#puts numero1
+try1 = Integer(gets.chomp)
+while try1 != numero1
+  puts "Wrong... Try again!"
+  time = time + 1
+  puts "#{time} tries until now\n\n"
+  try1 = Integer(gets.chomp)
+end
+if time <= 3
+  puts "Congrats! You advanced to Level 2 [0..10]"
+  time = 0
+else
+  puts "Sorry, you lost :("
+  puts "See you next time"
+  exit
+end
+numero2 = rand(0..10)
+#puts numero2
+try2 = Integer(gets.chomp)
+while try2 != numero2
+  puts "Wrong... Try again!"
+  time = time + 1
+  puts "#{time} number of tries until now\n\n"
+  try2 = Integer(gets.chomp)
+end
+if time <= 5
+  puts "Congratulations! You passed Level 2!!!"
+  puts "Time for Level 3 [10..50] 20 tries"
+  time = 0
+else
+  puts "Sorry but you lost at Level 2..."
+  puts "Better luck next time"
+  exit
+end
+numero3 = rand(10..50)
+#puts numero3
+try3 = Integer(gets.chomp)
+while try3 != numero3
+  puts "Wrong... Try again!"
+  time = time + 1
+  puts "#{time} number of tries until now\n\n"
+  try3 = Integer(gets.chomp)
+end
+if time <= 20
+  puts "YAY! Passed Level 3!"
+  puts "Time for Level 4 [100..200] (50 tries)"
+else
+  puts "Sorry you lost at Level 3."
+  puts "Better luck next time!"
+  exit
+end
+numero4 = rand(100..200)
+#puts numero4
+puts "And your guess is..."
+try4 = Integer(gets.chomp)
+while try4 != numero4
+  puts "Sorry! Try again!"
+  time = time + 1
+  puts "#{time} number of tries until now\n\n"
+  try4 = Integer(gets.chomp)
+end
+if time <= 50
+  puts "Congratulations! You did it!"
+  puts "You passed all of the levels :)"
+  puts "We are going to credit you a certificate of completion so we need your name and last name"
+  puts "What is your first name? You can change it if something is wrong later."
+  fname = String(gets.chomp)
+  puts "What is your last name? You can change it if something is wrong later."
+  lname = String(gets.chomp)
+  puts "Are you #{fname} #{lname}? (y/n)"
+  sure = String(gets.chomp)
+  if sure.capitalize == 'N'
+    while sure.capitalize == 'N'
+      puts "What is your first name?"
+      fname = String(gets.chomp)
+      puts "What is your last name?"
+      lname = String(gets.chomp)
+      puts "Are you #{fname} #{lname}?"
+      sure = String(gets.chomp)
+    end
+  end
+  if sure.capitalize == 'Y'
+    congr = "This certificate verifies that #{fname} #{lname} completed this number guessing game. You can try too!"
+    certificate = File.write("certificate_#{fname}_#{lname}.txt", congr)
+    puts "Certificate created successfully in running folder :)"
+    puts "Have fun :)"
+  end
+else
+  puts "Well, you made it to the final Level (4) but didn't beat it :(("
+  puts "Hope next time you beat it :)"
+  exit
+end
+exit
---
+++ README.md
@@ -0,0 +1,45 @@
+# Description
+Do you enjoy challenges based in probabilities, you want to test yourself, or waste some time?
+You just found a way to do so : Guess the number this program will choose. There are *4 levels*.
+Each of them have *increasing difficulty* and different number of tries available.
+
+# Usage
+There are no specifics other than knowing how to run a script from a terminal or command prompt
+If you don't here is how :
+1. Open a Terminal / CMD (UNIX/WINDOWS)
+2. Type 'ruby guessNum.rb' without the quotes
+3. Play
+
+# Requirements
+You need to install Ruby unless we release executable packages for all operating systems in the future (far distant future).
+You can get Ruby at https://www.ruby-lang.org/en/
+
+
+# Contributors
+Please, if anyone wants to, modify/add new levels. Contact me through GitHub or other ways I have mentioned in my Bio and I will add you as a contributor.
+
+
+Other tasks that would apply for contributors would be :
+
+1. Porting the script to .exe, and other executable and easy ways Linux and Apple users would be able to run this as an application.
+2. Fixing bugs wherever they appear.
+3. More tasks that will pop-up later.
+
+
+No matter your reason, send me a message stating the reason you would like to contribute and I will add you or keep you in mind for the future.
+
+#License
+
+Copyright 2021 CyberShadow7
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
